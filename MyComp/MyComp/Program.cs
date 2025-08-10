50d6df6cbf1f13b66dc518fb9dc543bc91070e2b
Nikita Goltsev <68222851+urmasa-tar@users.noreply.github.com>
2025-08-10 06:00:06 +0300

    clear_for_back

---
+++ MyComp/MyComp/Program.cs
@@ -0,0 +1,15 @@
+namespace MyComp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
